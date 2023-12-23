25fee7c811db8ce34612df9f217e0cf2bd23f617
Akbar7922 <Akbar.sarzehi@yahoo.com>
2023-12-23 19:18:25 +0330

    OK

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('test', function () {
+    return bcrypt('1234');
+});
 Route::get('/', function () {
     return view('welcome');
 })->middleware('auth');
